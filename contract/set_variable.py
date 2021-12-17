737d47450557764ba4f86545f12e6e6275e72db8
alienflip <73109076+alienflip@users.noreply.github.com>
2021-12-17 23:09:39 +0000

    Create set_variable.py

---
+++ contract/set_variable.py
@@ -0,0 +1,29 @@
+from pyteal import *
+
+def set_variable():
+
+   # global variable to be set    
+   handle_creation = Seq([
+       App.globalPut(Bytes("Variable"), Int(0)),
+       Return(Int(1))
+   ])
+
+   scratchCount = ScratchVar(TealType.uint64)
+
+   setVar = Seq([
+       scratchCount.store(App.globalGet(Bytes("Variable"))),
+       App.globalPut(Bytes("Variable"), Btoi(Txn.application_args[0])),
+       Return(Int(1))
+   ])
+
+   # boilderplate
+   program = Cond(
+       [Txn.application_id() == Int(0), handle_creation],
+       [Txn.on_completion() == OnComplete.OptIn, Return(Int(0))],
+       [Txn.on_completion() == OnComplete.CloseOut, Return(Int(0))],
+       [Txn.on_completion() == OnComplete.UpdateApplication, Return(Int(0))],
+       [Txn.on_completion() == OnComplete.DeleteApplication, Return(Int(0))],
+       [Txn.on_completion() == OnComplete.NoOp, setVar]
+   )
+   
+   return compileTeal(program, Mode.Application, version=5)
