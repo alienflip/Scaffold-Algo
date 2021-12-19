a98449c86e790006d3172b973905cbd2a4147c2b
alienflip <73109076+alienflip@users.noreply.github.com>
2021-12-19 00:00:13 +0000

    Update README.md

--- call/call.js
+++ call/call.js
@@ -1,5 +1,7 @@
 const algosdk = require('algosdk');
 const config = require('../scaffold-algo-config.json'); 
+const wait_confirm = require("./waitForConfirmation.js");
+const read_state = require("./readState.js");
 
 const client = new algosdk.Algodv2(
     token = {
@@ -12,40 +14,10 @@ const client = new algosdk.Algodv2(
 let mnemonic = "" // your mneumonic here
 let sender = algosdk.mnemonicToSecretKey(mnemonic);
 
-// read global state of contract
-async function readGlobalState(client, account, index){
-    let accountInfoResponse = await client.accountInformation(account).do();
-    for (let i = 0; i < accountInfoResponse['created-apps'].length; i++) { 
-        if (accountInfoResponse['created-apps'][i].id == index) {
-            console.log("Application's global state:");
-            for (let n = 0; n < accountInfoResponse['created-apps'][i]['params']['global-state'].length; n++) {
-                let resp = accountInfoResponse['created-apps'][i]['params']['global-state'][n];
-                const buff = Buffer.from(resp.key, 'base64');
-                console.log(buff.toString(), " : ", String.fromCharCode(resp.value.uint))
-            }
-        }
-    }
-}
-
-// wati for block confirmation
-const waitForConfirmation = async function (algodclient, txId) {
-    let status = (await algodclient.status().do());
-    let lastRound = status["last-round"];
-    while (true) {
-        const pendingInfo = await algodclient.pendingTransactionInformation(txId).do();
-        if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
-            //Got the completed Transaction
-            console.log("Transaction " + txId + " confirmed in round " + pendingInfo["confirmed-round"]);
-            break;
-        }
-        lastRound++;
-        await algodclient.statusAfterBlock(lastRound).do();
-    }
-}; 
-
-  // interact with contract
+// interact with contract
 async function change_contract_variable_to (new_variable, contractAddr, appId_){
-    await readGlobalState(client, contractAddr, appId_)
+    //await readGlobalState(client, contractAddr, appId_)
+    await read_state.readGlobalState(client, contractAddr, appId_);
 
     let params = await client.getTransactionParams().do();
 
@@ -65,9 +37,13 @@ async function change_contract_variable_to (new_variable, contractAddr, appId_){
     const signedCallTxn = callTxn.signTxn(sender.sk);
     const tx = await client.sendRawTransaction(signedCallTxn) .do();
 
-    await waitForConfirmation(client, tx.txId);
+    //await waitForConfirmation(client, tx.txId);
+    await wait_confirm.waitForConfirmation(client, tx.txId);
 
-    await readGlobalState(client, contractAddr, appId_)
+    //await readGlobalState(client, contractAddr, appId_);
+    await read_state.readGlobalState(client, contractAddr, appId_);
 };
 
-change_contract_variable_to(666, "", ); // your contract address and contract id here
+// remeber : the max integer you can put in the following
+// depends on how large you set "global_int" to in the deployment
+change_contract_variable_to(7, "", ); // your contract address and contract id here
